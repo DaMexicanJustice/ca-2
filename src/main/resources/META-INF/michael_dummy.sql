cd545c06bd5ab355ce5015433b0e01aa99f1b13c
Michael Veilis <Michael Veilis>
2016-10-03 15:23:17 +0200

    Have made info to Person and company

---
+++ src/main/resources/META-INF/michael_dummy.sql
@@ -0,0 +1,218 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Michael
+ * Created: 03-10-2016
+ */
+/* 50 */
+/* 50 */
+INSERT INTO infoentity (email) VALUES ("atest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Michael", "Veilis", 50);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Gaming", "Playing pc games", 50);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("29259979", "Cell phone", 50);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Rytterhaven 45", "In Farum", 50, 3520);
+/* 51 */
+INSERT INTO infoentity (email) VALUES ("btest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Riki", "Martin", 51);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Singing", "Singing songs", 51);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("13242133", "Cell phone", 51);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village A", "Village by the letter A", 51, 0800);
+/* 52 */
+INSERT INTO infoentity (email) VALUES ("ctest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Bob", "Marley", 52);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("High-Singing", "Singing while high on Hash", 52);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("53245123", "Cell phone", 52);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village B", "Village by the letter B", 52, 0877);
+/* 53 */
+INSERT INTO infoentity (email) VALUES ("dtest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("John", "Doe", 53);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Life", "Doing life", 53);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("65435678", "Cell phone", 53);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village C", "Village by the letter C", 53, 0892);
+/* 54 */
+INSERT INTO infoentity (email) VALUES ("etest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Patrick", "Desousa", 54);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Streaming", "Streaming Console Games", 54);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("56786562", "Cell phone", 54);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village D", "Village by the letter D", 54, 0918);
+/* 55 */
+INSERT INTO infoentity (email) VALUES ("ftest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Lasse", "Hansen", 55);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Coding", "Logical Coding", 55);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("12341453", "Cell phone", 55);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village E", "Village by the letter E", 55, 1002);
+/* 56 */
+INSERT INTO infoentity (email) VALUES ("gtest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Christian", "Jacobsen", 56);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Work", "Working with security", 56);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("12376567", "Cell phone", 56);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village F", "Village by the letter F", 56, 1003);
+/* 57 */
+INSERT INTO infoentity (email) VALUES ("htest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Adolf", "Hitler", 57);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Dictator", "Dictator of Nazi Germany", 57);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("12348909", "Cell phone", 57);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village G", "Village by the letter G", 57, 1001);
+/* 58 */
+INSERT INTO infoentity (email) VALUES ("itest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Joseph", "Stalin", 58);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Dictator", "Dicator of Soviet-Russia", 58);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("12765678", "Cell phone", 58);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village H", "Village by the letter H", 58, 1004);
+/* 59 */
+INSERT INTO infoentity (email) VALUES ("jtest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Anders", "Fogh", 59);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("General Secretary", "General Secretary of Nasa", 59);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("12388878", "Cell phone", 59);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village I", "Village by the letter I", 59, 1005);
+/* 60 */
+INSERT INTO infoentity (email) VALUES ("ktest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Lars", "Løkke", 60);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("PrimeMinister", "PrimeMinister of Denmark", 60);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("12347778", "Cell phone", 60);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village J", "Village by the letter J", 60, 1006);
+/* 61 */
+INSERT INTO infoentity (email) VALUES ("ltest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Anders", "Matthesen", 61);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Comedian", "Proffesional standup comedian", 61);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("15555678", "Cell phone", 61);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village K", "Village by the letter K", 61, 1007);
+/* 62 */
+INSERT INTO infoentity (email) VALUES ("mtest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Anders", "And", 62);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Andersine", "Har det sjovt med Andersine", 62);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("12344448", "Cell phone", 62);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Paradisæblevej 111", "Byen hvor Anders And bor", 62, 1008);
+/* 63 */
+INSERT INTO infoentity (email) VALUES ("ntest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Andersine", "And", 63);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Anders", "Har det sjovt med Anders", 63);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("16765678", "Cell phone", 63);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village M", "Village by the letter M", 63, 1009);
+/* 64 */
+INSERT INTO infoentity (email) VALUES ("otest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Harambe", "TheGorilla", 64);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Dead", "Shot to death by a man", 64);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("12345555", "Cell phone", 64);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village N", "Village by the letter N", 64, 1010);
+/* 65 */
+INSERT INTO infoentity (email) VALUES ("ptest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Kappa", "Twitch", 65);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Kappa emoting", "The almighty emote on twitch", 65);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("66666666", "Cell phone", 65);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village O", "Village by the letter O", 65, 1011);
+/* 66 */
+INSERT INTO infoentity (email) VALUES ("qtest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Alexander", "The Great", 66);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Conquering", "Conquering countries", 66);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("12344343", "Cell phone", 66);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village P", "Village by the letter P", 66, 1012);
+/* 67 */
+INSERT INTO infoentity (email) VALUES ("rtest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Mulan", "Ching-Chong", 67);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Chinese Soldier", "Being a chinese soldier in love", 67);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("12344444", "Cell phone", 67);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village Q", "Village by the letter Q", 67, 1013);
+/* 68 */
+INSERT INTO infoentity (email) VALUES ("stest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Master", "Chief", 68);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Super soldier", "Almighty super soldier who always respawns after death", 68);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("11115678", "Cell phone", 68);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village R", "Village by the letter R", 68, 1014);
+/* 69 */
+INSERT INTO infoentity (email) VALUES ("ttest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Varian", "Wrynn", 69);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("King", "King of StormWind", 69);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("12311178", "Cell phone", 69);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village S", "Village by the letter S", 69, 1015);
+/* 70 */
+INSERT INTO infoentity (email) VALUES ("utest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Anduin", "Wrynn", 70);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Prince", "Prince of StormWind", 70);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("12341328", "Cell phone", 70);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village T", "Village by the letter T", 70, 1016);
+/* 71 */
+INSERT INTO infoentity (email) VALUES ("vtest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Illidan", "Stormrage", 71);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Betrayer", "Betraying people....duh", 71);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("12341441", "Cell phone", 71);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village U", "Village by the letter U", 71, 1017);
+/* 72 */
+INSERT INTO infoentity (email) VALUES ("xtest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Laura", "Bodevig", 72);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Riding", "Riding horses(and dudes)", 72);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("12555668", "Cell phone", 72);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village V", "Village by the letter V", 72, 1018);
+/* 73 */
+INSERT INTO infoentity (email) VALUES ("ytest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Lars", "Larsen", 73);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Driving", "Driving a bus", 73);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("98765432", "Cell phone", 73);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village X", "Village by the letter X", 73, 1019);
+/* 74 */
+INSERT INTO infoentity (email) VALUES ("ztest@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Lotte", "HvorErDuHenne", 74);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Hiding", "Hiding from everyone", 74);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("99333222", "Cell phone", 74);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village Y", "Village by the letter Y", 74, 1020);
+/* 75 */
+INSERT INTO infoentity (email) VALUES ("1test@tester.dk");
+INSERT INTO person (firstName, lastName, fk_id) VALUES ("Pipi", "Langstrømpe", 75);
+INSERT INTO hobby (hobbyName, description, fk_id) VALUES ("Parentless", "No parents but have a horse and a monkey", 75);
+INSERT INTO phone(pnum, description, fk_id) VALUES ("11111119", "Cell phone", 75);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village Z", "Village by the letter Z", 75, 1021);
+
+
+/* 11 */
+INSERT INTO infoentity (email) VALUES ("a1test@tester.dk");
+INSERT INTO company (cvr, cname, description, no_of_employees, market_value, fk_id) VALUES ("76787678", "companyA","company with the letter A", 10, 5000, 11);  
+INSERT INTO phone(pnum, description, fk_id) VALUES ("12444222", "Cell phone", 11);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village 1", "Village that likes the number 1", 11, 1022);
+/* 12 */
+INSERT INTO infoentity (email) VALUES ("a1test@tester.dk");
+INSERT INTO company (cvr, cname, description, no_of_employees, market_value, fk_id) VALUES ("76782678", "companyB","company with the letter B", 50, 10000, 12);  
+INSERT INTO phone(pnum, description, fk_id) VALUES ("12323238", "Cell phone", 12);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village 2", "Village that likes the number 2", 12, 1023);
+/* 13 */
+INSERT INTO infoentity (email) VALUES ("a1test@tester.dk");
+INSERT INTO company (cvr, cname, description, no_of_employees, market_value, fk_id) VALUES ("80900908", "companyC","company with the letter C", 5, 2500, 13);  
+INSERT INTO phone(pnum, description, fk_id) VALUES ("33335678", "Cell phone", 13);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village 3", "Village that likes the number 3", 13, 1024);
+/* 14 */
+INSERT INTO infoentity (email) VALUES ("a1test@tester.dk");
+INSERT INTO company (cvr, cname, description, no_of_employees, market_value, fk_id) VALUES ("70001378", "companyD","company with the letter D", 560, 555000, 14);  
+INSERT INTO phone(pnum, description, fk_id) VALUES ("43215678", "Cell phone", 14);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village 4", "Village that likes the number 4", 14, 1025);
+/* 15 */
+INSERT INTO infoentity (email) VALUES ("a1test@tester.dk");
+INSERT INTO company (cvr, cname, description, no_of_employees, market_value, fk_id) VALUES ("22287338", "companyE","company with the letter E", 410, 445000, 15);  
+INSERT INTO phone(pnum, description, fk_id) VALUES ("45545678", "Cell phone", 15);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village 5", "Village that likes the number 5", 15, 1026);
+/* 16 */
+INSERT INTO infoentity (email) VALUES ("a1test@tester.dk");
+INSERT INTO company (cvr, cname, description, no_of_employees, market_value, fk_id) VALUES ("06223678", "companyF","company with the letter F", 23, 52000, 16);  
+INSERT INTO phone(pnum, description, fk_id) VALUES ("12345555", "Cell phone", 16);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village 6", "Village that likes the number 6", 16, 1027);
+/* 17 */
+INSERT INTO infoentity (email) VALUES ("a1test@tester.dk");
+INSERT INTO company (cvr, cname, description, no_of_employees, market_value, fk_id) VALUES ("76009978", "companyG","company with the letter G", 16, 6000, 17);  
+INSERT INTO phone(pnum, description, fk_id) VALUES ("12766678", "Cell phone", 17);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village 7", "Village that likes the number 7", 17, 1028);
+/* 18 */
+INSERT INTO infoentity (email) VALUES ("a1test@tester.dk");
+INSERT INTO company (cvr, cname, description, no_of_employees, market_value, fk_id) VALUES ("11187678", "companyH","company with the letter H", 2, 500, 18);  
+INSERT INTO phone(pnum, description, fk_id) VALUES ("32245678", "Cell phone", 18);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village 8", "Village that likes the number 8", 18, 1029);
+/* 19 */
+INSERT INTO infoentity (email) VALUES ("a1test@tester.dk");
+INSERT INTO company (cvr, cname, description, no_of_employees, market_value, fk_id) VALUES ("00987678", "companyI","company with the letter I", 180, 590000, 19);  
+INSERT INTO phone(pnum, description, fk_id) VALUES ("12345898", "Cell phone", 19);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village 9", "Village that likes the number 9", 19, 1030);
+/* 20 */
+INSERT INTO infoentity (email) VALUES ("a1test@tester.dk");
+INSERT INTO company (cvr, cname, description, no_of_employees, market_value, fk_id) VALUES ("76989678", "companyJ","company with the letter j", 110, 125000, 20);  
+INSERT INTO phone(pnum, description, fk_id) VALUES ("22322278", "Cell phone", 20);
+INSERT INTO address (street, additionalinfo, fk_id, fk_zipcode) VALUES ("Village 10", "Village that likes the number 10", 20, 1031);
