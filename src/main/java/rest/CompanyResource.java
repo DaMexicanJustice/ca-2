61b76c026bc4ebd5c4f93561b67d534b8b9a67d9
KongGrethe <christiandealpost@hotmail.com>
2016-10-05 11:12:25 +0200

    get companies method created

--- src/main/java/rest/CompanyResource.java
+++ src/main/java/rest/CompanyResource.java
@@ -11,6 +11,7 @@ import entity.Company;
 import exception.error.CompanyNotFoundException;
 import facade.Facade;
 import facade.IFacade;
+import java.util.List;
 import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
 import javax.ws.rs.core.Context;
@@ -18,6 +19,7 @@ import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -48,9 +50,9 @@ public class CompanyResource {
     }
 
     @GET
-    @Path("complete/{cvr}")
+    @Path("comp/all/{cvr}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getCompanyByCvr(String cvr) throws CompanyNotFoundException {
+    public String getCompanyByCvr(@PathParam("cvr") String cvr) throws CompanyNotFoundException {
         try {
             Company company = facade.getCompanyByCvr(cvr);
             return jsonC.CompanyToJSON(company);
@@ -58,5 +60,12 @@ public class CompanyResource {
             throw new CompanyNotFoundException("No company with that cvr");
         }
     }
-
+    
+    @GET
+    @Path("comp/all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getCompanies(){
+            List<Company> allCompanies = facade.getCompanies();
+            return jsonC.CompaniesContactInfoToJSON(allCompanies);
+    }
 }
