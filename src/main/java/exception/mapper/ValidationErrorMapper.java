64669e878d9a514340efd6aad03783afa6398b58
Patrick Johansen <Patrick Johansen>
2016-10-05 12:12:53 +0200

    Implemented validation error mapper

--- src/main/java/exception/mapper/ValidationErrorMapper.java
+++ src/main/java/exception/mapper/ValidationErrorMapper.java
@@ -5,10 +5,29 @@
  */
 package exception.mapper;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import exception.error.ValidationErrorException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import org.glassfish.jersey.internal.util.ExceptionUtils;
+
 /**
  *
  * @author xboxm
  */
-public class ValidationErrorMapper {
+public class ValidationErrorMapper implements ExceptionMapper<ValidationErrorException> {
+    
+    static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    @Override
+    public Response toResponse(ValidationErrorException exception) {
+        com.google.gson.JsonObject job = new JsonObject();
+        job.addProperty("status", 400);
+        job.addProperty("msg", exception.getMessage());
+        job.addProperty("stackTrace", ExceptionUtils.exceptionStackTraceAsString(exception));
+        return Response.status(400).entity(gson.toJson(job)).build();
+    }
     
 }
