1f8c58686e26385b5e12f9252fb6309a828d951f
KongGrethe <christiandealpost@hotmail.com>
2016-10-05 10:11:21 +0200

    companyResource

--- src/main/java/rest/CompanyResource.java
+++ src/main/java/rest/CompanyResource.java
@@ -5,13 +5,19 @@
  */
 package rest;
 
+import converter.IJSONConverter;
+import converter.JSONConverter;
+import entity.Company;
+import exception.error.CompanyNotFoundException;
+import facade.Facade;
+import facade.IFacade;
+import javax.persistence.NoResultException;
+import javax.persistence.Persistence;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -25,14 +31,32 @@ public class CompanyResource {
     @Context
     private UriInfo context;
 
+    private IFacade facade;
+    private IJSONConverter jsonC;
+
     /**
      * Creates a new instance of CompanyResource
      */
     public CompanyResource() {
+        facade = new Facade(Persistence.createEntityManagerFactory("deploy"));
+        jsonC = new JSONConverter();
     }
-    
+
     @GET
     public String getText() {
         return "Hello World";
     }
+
+    @GET
+    @Path("complete/{cvr}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getCompanyByCvr(String cvr) throws CompanyNotFoundException {
+        try {
+            Company company = facade.getCompanyByCvr(cvr);
+            return jsonC.CompanyToJSON(company);
+        } catch (NoResultException ex) {
+            throw new CompanyNotFoundException("No company with that cvr");
+        }
+    }
+
 }
